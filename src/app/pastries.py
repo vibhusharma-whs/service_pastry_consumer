ba20c96567bfbbcc52df4527ea4fc2f68a39dceb
vibhusharma-whs <vibhusharma711@gmail.com>
2021-07-08 22:05:07 -0500

    service_pastry_consumer

--- src/app/pastries.py
+++ src/app/pastries.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request
 
 # import json to load JSON data to a python dictionary
 import json
-import requests
 
 # urllib.request to make a request to api
-import urllib.request
+import requests
 
 
 app = Flask(__name__)
@@ -14,12 +13,12 @@ app = Flask(__name__)
 def finding():
 	# source contain json data from api
 	# communication from consumer service to fast API server here
-	response = requests.get('http://phresh_server_1:8000/api/findings/')
+	response = requests.get('http://service_pastry/pastries')
 
 	# converting JSON data to a dictionary
 	list_of_data = json.loads(response)
 	print(list_of_data)
-	data = {"finding_class_id":list_of_data[1]}
+	data = {"pastries":list_of_data}
 	return render_template('index.html', data = data)
 	
 
