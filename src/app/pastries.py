eda3d3c40d40164797d6b9b1555e557cb359368f
vibhusharma-whs <vibhusharma711@gmail.com>
2021-07-13 18:03:22 -0500

    Final changes with deployment

--- src/app/pastries.py
+++ src/app/pastries.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template, request
 
 # import json to load JSON data to a python dictionary
+import os
 import json
 import urllib.request
 
-# urllib.request to make a request to api
-import requests
-
 
 app = Flask(__name__)
-
+hostname = os.environ.get("NODE_IP")
 @app.route('/', methods =['GET'])
 def finding():
 	# source contain json data from api
 	# communication from consumer service to fast API server here
-	response = urllib.request.urlopen('http://service_pastry/pastries').read()
+	response = urllib.request.urlopen('http://'+hostname+':5000/pastries').read()
 	# converting JSON data to a dictionary
 	list_of_data = json.loads(response)
 	data = {"pastries":list_of_data}
